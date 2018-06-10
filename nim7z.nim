aab5f2148c76ad0a4e43e2fb452026070e796f6f
Ganesh Viswanathan <dev@genotrance.com>
2018-06-10 16:41:54 -0500

    Fix destructor

--- nim7z.nim
+++ nim7z.nim
@@ -9,7 +9,8 @@ template SzArEx_IsDir(p, i: untyped): untyped =
   SzBitArray_Check(p.IsDirs, i)
 
 type
-  SvnzFile = ref object
+  SvnzFile* = ref SvnzFileObj
+  SvnzFileObj = object
     filename: string
     archiveStream: CFileInStream
     lookStream: CLookToRead2
@@ -103,12 +104,11 @@ iterator walk(svnz: SvnzFile): tuple[filename: string, isdir: int, contents: ptr
 
   checkError()
 
-when defined(windows):
-  proc `=destroy`[SvnzFile](svnz: var SvnzFile) =
-    SzArEx_Free(addr svnz.db, addr allocImp)
-    allocImp.Free(addr allocImp, svnz.lookStream.buf)
+proc `=destroy`(svnz: var SvnzFileObj) =
+  SzArEx_Free(addr svnz.db, addr allocImp)
+  allocImp.Free(addr allocImp, svnz.lookStream.buf)
 
-    discard File_Close(addr svnz.archiveStream.file)
+  discard File_Close(addr svnz.archiveStream.file)
 
 proc extract*(svnz: SvnzFile, directory: string, skipOuterDirs=false, tempDir: string = nil) =
   ## Extracts the files stored in the opened ``SvnzFile`` into the specified
